383c23f6afb81dc235257b66e09a6e6caf692259
NOMAN1802 <mustakimnoman82@gmail.com>
2024-07-03 18:24:32 +0600

    initial setup and routing

---
+++ src/pages/admin/CreateAdmin.tsx
@@ -0,0 +1,9 @@
+const CreateAdmin = () => {
+    return (
+        <div>
+            <h1>Create Admin component</h1>
+        </div>
+    );
+};
+
+export default CreateAdmin;
---
+++ src/pages/admin/CreateFaculty.tsx
@@ -0,0 +1,9 @@
+const CreateFaculty = () => {
+    return (
+        <div>
+            <h1>Create faculty component</h1>
+        </div>
+    );
+};
+
+export default CreateFaculty;

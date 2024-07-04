d746bf8b67aba8d6a6d9d66cb24ad5d6c2d5e382
NOMAN1802 <mustakimnoman82@gmail.com>
2024-07-04 23:01:22 +0600

    authentication done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,36 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
   
-
+    const dispatch = useDispatch()
     const {register, handleSubmit} = useForm({
         defaultValues: {
          userId: '0001',
          password: 'admin12345',
         }
     }); 
-    const [login, {data,error }] = useLoginMutation();
+    const [login, { error }] = useLoginMutation();
 
-    console.log("data ==>", data);
+    
     console.log("error ==>", error);
-    const onSubmit = (data) =>{
+
+    const onSubmit = async (data) =>{
 
         const userInfo = {
             id: data.userId,
             password: data.password,
         }
-        login(userInfo)
-     console.log(userInfo);
+     const res = await  login(userInfo).unwrap();
+
+     const user = verifyToken(res.data.accessToken);
+     console.log(user);
+     dispatch(setUser({user: user, token: res.data.accessToken}));
+     console.log(res);
     }
 
     return (
